b7ac8bced87ffc48c09ed21ededd6a9f935f7437
Daniel Seidler <daniel.seidler@gmail.com>
2022-12-26 23:09:29 +0100

    new design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://danseid.github.io',
-	base: '/me',
-	integrations: [mdx(), sitemap()],
+  site: 'https://danseid.github.io',
+  base: '/me',
+  integrations: [mdx(), sitemap(), tailwind()]
 });
